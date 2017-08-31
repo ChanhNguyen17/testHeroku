6574bf9fdc0faf67baf93396dcf541297314e5e6
Chanh Nguyen <chanhnguyen171@gmail.com>
2017-08-31 13:16:38 +0300

    process env test_var heroku

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: TEST_VAR });
+  res.render('index', { title: process.env.TEST_VAR });
 });
 
 module.exports = router;
